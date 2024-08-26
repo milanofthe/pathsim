9f44b3c4704ed96cfa45a6f2e14925a4db19c700
milanofthe <milan.rother@gmx.de>
2024-08-26 15:27:42 +0200

    adding tests for error checking

--- tests/utils/test_funcs.py
+++ tests/utils/test_funcs.py
@@ -14,7 +14,10 @@ import numpy as np
 from pathsim.utils.funcs import (
     dict_to_array, 
     array_to_dict, 
-    rel_error, 
+    abs_error,
+    rel_error,
+    max_error,
+    max_error_dicts,
     max_rel_error, 
     max_rel_error_dicts,
     numerical_jacobian,
@@ -29,6 +32,14 @@ class TestUtilsFuncs(unittest.TestCase):
     test all the array-dict conversions and functions for error calculation
     """
 
+    def test_abs_error(self):
+
+        self.assertEqual(abs_error(1.0, 2.0), 1.0)    
+        self.assertEqual(abs_error(2.0, 1.0), 1.0)
+        self.assertEqual(abs_error(2.0, 0.0), 2.0)
+        self.assertEqual(abs_error(0.0, 1.0), 1.0)
+
+
     def test_rel_error(self):
 
         #test fallback to abs error
@@ -40,6 +51,12 @@ class TestUtilsFuncs(unittest.TestCase):
         
         #test a>b
         self.assertEqual(rel_error(2.0, 1.0), 0.5)
+
+
+    def test_max_error(self):
+        self.assertEqual(max_error([0.00139, 2.4, 87, 1, 97.8, 4.35], 
+                                   [1.00139, 1.4, 86, 2, 98.2, 33.35]), 
+                         29)
 
 
     def test_max_rel_error(self):
@@ -75,6 +92,11 @@ class TestUtilsFuncs(unittest.TestCase):
         self.assertEqual(max_rel_error_dicts({0:0.00139, 1:2.4, 2:87, 3:1, 4:97.8, 5:4.35}, 
                                              {0:1.00139, 1:1.4, 2:86, 3:2, 4:98.2, 5:33.35}), 
                          1/0.00139)
+
+    def test_max_error_dicts(self):
+        self.assertEqual(max_error_dicts({0:0.00139, 1:2.4, 2:87, 3:1, 4:97.8, 5:4.35}, 
+                                             {0:1.00139, 1:1.4, 2:86, 3:2, 4:98.2, 5:33.35}), 
+                         29)
 
 
 class TestJacobian(unittest.TestCase):
