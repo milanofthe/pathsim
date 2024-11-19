906b191af03f46e04d2fc322da9ad3711a794d6a
milanofthe <milan.rother@gmx.de>
2024-11-19 22:12:27 +0100

    more tests for zero crossing

--- tests/events/test_zerocrossing.py
+++ tests/events/test_zerocrossing.py
@@ -1,7 +1,7 @@
 ########################################################################################
 ##
-##                                  TESTS FOR 
-##                              'events._event.py'
+##                                   TESTS FOR 
+##                            'events.zerocrossing.py'
 ##
 ##                               Milan Rother 2024
 ##
@@ -12,7 +12,11 @@
 import unittest
 import numpy as np
 
-from pathsim.events.zerocrossing import ZeroCrossing
+from pathsim.events.zerocrossing import (
+    ZeroCrossing, 
+    ZeroCrossingUp, 
+    ZeroCrossingDown
+    )
 
 from pathsim.blocks._block import Block
 
@@ -116,7 +120,147 @@ class TestZeroCrossing(unittest.TestCase):
         self.assertEqual(ra, 1/2)
 
 
+class TestZeroCrossingUp(unittest.TestCase):
+    """
+    Test the implementation of the 'ZeroCrossingUp' event class.
+    """
 
+    def test_detect_up(self):
+
+        #upwards
+        e = ZeroCrossingUp(func_evt=lambda y, x, t: t-2)
+
+        #before crossing
+        e.buffer(0)
+        de, cl, ra = e.detect(1)
+        self.assertFalse(de)
+        self.assertFalse(cl)
+        self.assertEqual(ra, 1)
+
+        #crossing 1
+        de, cl, ra = e.detect(3)
+        self.assertTrue(de)
+        self.assertFalse(cl)
+        self.assertEqual(ra, 2/3)
+
+        #crossing 2
+        e.buffer(1)
+        de, cl, ra = e.detect(3)
+        self.assertTrue(de)
+        self.assertFalse(cl)
+        self.assertEqual(ra, 1/2)
+
+        #after crossing
+        e.buffer(3)
+        de, cl, ra = e.detect(4)
+        self.assertFalse(de)
+        self.assertFalse(cl)
+        self.assertEqual(ra, 1)
+
+
+    def test_detect_down(self):
+
+        #downwards
+        e = ZeroCrossingUp(func_evt=lambda y, x, t: 2-t)
+
+        #before crossing
+        e.buffer(0)
+        de, cl, ra = e.detect(1)
+        self.assertFalse(de)
+        self.assertFalse(cl)
+        self.assertEqual(ra, 1)
+
+        #crossing 1 -> not triggering
+        de, cl, ra = e.detect(3)
+        self.assertFalse(de)
+        self.assertFalse(cl)
+        self.assertEqual(ra, 1)
+
+        #crossing 2 -> not triggering
+        e.buffer(1)
+        de, cl, ra = e.detect(3)
+        self.assertFalse(de)
+        self.assertFalse(cl)
+        self.assertEqual(ra, 1)
+
+        #after crossing
+        e.buffer(3)
+        de, cl, ra = e.detect(4)
+        self.assertFalse(de)
+        self.assertFalse(cl)
+        self.assertEqual(ra, 1)
+
+
+
+class TestZeroCrossingDown(unittest.TestCase):
+    """
+    Test the implementation of the 'ZeroCrossingDown' event class.
+    """
+
+    def test_detect_up(self):
+
+        #upwards
+        e = ZeroCrossingDown(func_evt=lambda y, x, t: t-2)
+
+        #before crossing
+        e.buffer(0)
+        de, cl, ra = e.detect(1)
+        self.assertFalse(de)
+        self.assertFalse(cl)
+        self.assertEqual(ra, 1)
+
+        #crossing 1 -> not triggering
+        de, cl, ra = e.detect(3)
+        self.assertFalse(de)
+        self.assertFalse(cl)
+        self.assertEqual(ra, 1)
+
+        #crossing 2 -> not triggering
+        e.buffer(1)
+        de, cl, ra = e.detect(3)
+        self.assertFalse(de)
+        self.assertFalse(cl)
+        self.assertEqual(ra, 1)
+
+        #after crossing
+        e.buffer(3)
+        de, cl, ra = e.detect(4)
+        self.assertFalse(de)
+        self.assertFalse(cl)
+        self.assertEqual(ra, 1)
+
+
+    def test_detect_down(self):
+
+        #downwards
+        e = ZeroCrossingDown(func_evt=lambda y, x, t: 2-t)
+
+        #before crossing
+        e.buffer(0)
+        de, cl, ra = e.detect(1)
+        self.assertFalse(de)
+        self.assertFalse(cl)
+        self.assertEqual(ra, 1)
+
+        #crossing 1
+        de, cl, ra = e.detect(3)
+        self.assertTrue(de)
+        self.assertFalse(cl)
+        self.assertEqual(ra, 2/3)
+
+        #crossing 2
+        e.buffer(1)
+        de, cl, ra = e.detect(3)
+        self.assertTrue(de)
+        self.assertFalse(cl)
+        self.assertEqual(ra, 1/2)        
+
+        #after crossing
+        e.buffer(3)
+        de, cl, ra = e.detect(4)
+        self.assertFalse(de)
+        self.assertFalse(cl)
+        self.assertEqual(ra, 1)
 
 
 
