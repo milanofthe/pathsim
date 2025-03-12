08352b272e138e26bde3a698e7604c5d9fbf6907
Milan Rother <milan.rother@gmx.de>
2025-03-12 20:52:33 +0100

    improved adaptive buffer

--- pathsim/utils/adaptivebuffer.py
+++ pathsim/utils/adaptivebuffer.py
@@ -18,9 +18,9 @@ from bisect import bisect_left
 
 class AdaptiveBuffer:
     """A class that manages an adaptive buffer for delay modeling which is primarily 
-    used in the pathsim 'Delay' block. 
+    used in the pathsim 'Delay' block but might have future applications aswell.
 
-    It implements a linear interpolation for arbitraty time lookup.
+    It implements a linear interpolation for arbitrary time lookup.
     
     Parameters
     ----------
@@ -29,27 +29,22 @@ class AdaptiveBuffer:
     
     Attributes
     ----------
-    buffer : deque
-        deque that collects the data for buffering
-    counter : int
-        count the number of lookups
-    clean_every : int
-        interval for buffer cleanup
+    buffer_t : deque
+        deque that collects the time data for buffering
+    buffer_v : deque
+        deque that collects the value data for buffering
     """
 
     def __init__(self, delay):
 
         #the buffer uses a double ended queue
-        self.buffer = deque()
+        self.buffer_t = deque()
+        self.buffer_v = deque()
         self.delay = delay
-
-        #for buffer cleanup every 100 lookups
-        self.clean_every = 100
-        self.counter = 0
 
 
     def __len__(self):
-        return len(self.buffer)
+        return len(self.buffer_t)
 
 
     def add(self, t, value):
@@ -64,51 +59,64 @@ class AdaptiveBuffer:
         """
 
         #add the time-value tuple
-        self.buffer.append((t, value))
+        self.buffer_t.append(t)
+        self.buffer_v.append(value)
         
-        #clean up the buffer
-        if self.counter > self.clean_every:
-            self.counter = 0
-            while len(self.buffer) > 1 and t > self.delay + self.buffer[0][0] :
-                self.buffer.popleft()
-        else:
-            self.counter += 1
+        #remove second to last value from buffer -> enable interpolation
+        if len(self.buffer_t) > 1:
+            while t - self.buffer_t[1] > self.delay:
+                self.buffer_t.popleft()
+                self.buffer_v.popleft()
 
-    
-    def get(self, t):
-        """lookup datapoint from buffer
+
+    def interp(self, t):
+        """interpolate buffer at defined lookup time
     
         Parameters
         ----------
         t : float
-            time for lookup
+            time for interpolation
+
+        Returns
+        -------
+        out : float, array
+            interpolated value
         """
 
         #default 0
-        if not self.buffer:
+        if not self.buffer_t:
             return 0.0
-        
-        #interpolation
-        target_time = t - self.delay
 
         #requested time too small -> return first value
-        if target_time <= self.buffer[0][0]:
-            return self.buffer[0][1]
+        if t <= self.buffer_t[0]:
+            return self.buffer_v[0]
         
         #requested time too large -> return last value
-        if target_time >= self.buffer[-1][0]:
-            return self.buffer[-1][1]
+        if t >= self.buffer_t[-1]:
+            return self.buffer_v[-1]
 
         #find buffer index for requested time
-        i = bisect_left(self.buffer, (target_time,))
-        t0, y0 = self.buffer[i-1]
-        t1, y1 = self.buffer[i]
-        
+        i = bisect_left(self.buffer_t, t)
+        t0, t1 = self.buffer_t[i], self.buffer_t[i-1]
+        y0, y1 = self.buffer_v[i], self.buffer_v[i-1]
+    
         #linear interpolation
-        return y0 + (y1 - y0) * (target_time - t0) / (t1 - t0)
+        return y0 + (y1 - y0) * (t - t0) / (t1 - t0)
+
+
+    def get(self, t):
+        """lookup datapoint from buffer with 
+        delay at `t_lookup = t - delay`
+    
+        Parameters
+        ----------
+        t : float
+            time for lookup with delay
+        """
+        return self.interp(t - self.delay)
 
 
     def clear(self):
         """clear the buffer, reset everything"""
-        self.buffer = deque()
-        self.counter = 0
+        self.buffer_t.clear()
+        self.buffer_v.clear()
