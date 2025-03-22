7d009f0b1ae5ea09e51e18d98a9533f14de11edc
Milan Rother <milan.rother@gmx.de>
2025-03-22 10:27:11 +0100

    updated the 'Adder' block to now accept operations to be applied at the inputs as a string, i.e. '+-' for convenience, default remains regular sum

--- pathsim/blocks/adder.py
+++ pathsim/blocks/adder.py
@@ -20,19 +20,81 @@ from ..optim.operator import Operator
 # MISO BLOCKS ===========================================================================
 
 class Adder(Block):
-    """Summs / adds all input signals (MISO)
+    """Summs / adds up all input signals to a single output signal (MISO)
     
+    This is how it works in the default case
+
     .. math::
         
         y(t) = \\sum_i u_i(t)
 
+
+    and like this when additional operations are defined
+
+    .. math::
+        
+        y(t) = \\sum_i \\mathrm{op}_i \\cdot u_i(t)
+    
+
+    Example
+    -------
+
+    This is the default initialization that just adds up all the inputs:
+
+    .. code-block:: python
+
+        A = Adder()
+
+
+    and this is the initialization with specific operations that subtracts 
+    the second from first input and neglects all others:
+
+    .. code-block:: python
+    
+        A = Adder('+-')
+
+
+    Parameters
+    ----------
+    operations : str, optional
+        optional string of operations to be applied before 
+        summation, i.e. '+-' will compute the difference, 
+        'None' will just perform regular sum
+    
+    
+    Attributes
+    ----------
+    _ops : dict
+        dict that maps string operations to numerical
+    op_alg : Operator
+        internal algebraic operator
     """
-    def __init__(self):
+
+    def __init__(self, operations=None):
         super().__init__()
 
+        #allowed arithmetic operations
+        self._ops = {"+":1.0, "-":-1.0, "0":0.0}
+
+        #input validation
+        if operations is not None:
+            if not isinstance(operations, str):
+                raise ValueError("'operations' must be string or 'None'")
+            for op in operations:
+                if op not in self._ops:
+                    raise ValueError(f"operation '{op}' not in {self._ops}")
+        
+        self.operations = operations    
+
+        def sum_ops(X):
+            return sum(x*self._ops[op] for op, x in zip(self.operations, X))
+        def jac_ops(X):
+            return np.array([self._ops[op] for op in self.operations])
+
+        #create internal algebraic operator
         self.op_alg = Operator(
-            func=lambda x: sum(x), 
-            jac=lambda x: np.ones(len(x))
+            func=sum if self.operations is None else sum_ops, 
+            jac=lambda x: np.ones(len(x)) if self.operations is None else jac_ops
             )
 
 
