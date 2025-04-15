e9cec7df8c37742c2f28028d7cb060f9157c4c41
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2025-04-14 20:11:15 -0400

    fixed version in docs conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,7 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
 
 
-from pathsim._version import __version__ 
+from pathsim import __version__ 
 
 # -- Project information -----------------------------------------------------
 
