693c4031d5c1197cfd1e0ce72b47d969d3a36d93
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2025-08-03 20:00:55 -0400

    removed unused import

--- src/pathsim/blocks/scope.py
+++ src/pathsim/blocks/scope.py
@@ -17,8 +17,6 @@ import warnings
 import numpy as np
 import matplotlib.pyplot as plt
 
-from mpl_toolkits.mplot3d import Axes3D
-
 from ._block import Block
 from ..utils.realtimeplotter import RealtimePlotter
 
